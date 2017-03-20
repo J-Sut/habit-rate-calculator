dddd24b9d0ce92d60a2f40c95a8bc4dfb1699a5f
j-Sut <flowstategame@gmail.com>
2017-03-20 13:46:44 -0700

    refined getValue functions

--- app.js
+++ app.js
@@ -3,8 +3,7 @@
 
 //2 functions that modify state
 
-function getDaysCounting() {
-	result = Number($('.days-counting').val());
+/*	result = Number($('.days-counting').val());
 	return result;
 };
 
@@ -16,25 +15,35 @@ function getDaysPracticed(){
 function getDesiredPercentage() {
 	result = Number($('.desired-percent').val());
 	return result;
+}; */
+
+function getValue(from){
+	value = Number($(from).val());
+	return value;
 };
 
-//3. functions that render state
 function calculateNeededStreak(daysCounting, daysPracticed, desiredPercent, initDaysCounting) {
 	if (daysPracticed/daysCounting < desiredPercent) {
 		daysCounting += 1;
 		daysPracticed += 1;
 		calculateNeededStreak(daysCounting, daysPracticed, desiredPercent, initDaysCounting);
 	} else {
-		$('.days-display').removeClass('hidden');
-		$('.days-display').text("To reach " + desiredPercent + ", you will need to practice for " + (daysCounting - initDaysCounting) + " days in a row.");
+		renderState(daysCounting, desiredPercent, initDaysCounting);
 	}
+};
+
+//3. functions that render state
+function renderState(daysCounting, desiredPercent, initDaysCounting){
+
+		$('.days-display').removeClass('hidden');
+		$('.days-display').text("To reach " + desiredPercent + ", you will need to practice for " + (daysCounting - initDaysCounting) + " days in a row.")
 };
 
 //4. Event listeners
 $('#checkButton').click(function() {
-	var daysCounting = getDaysCounting();
+	var daysCounting = getValue("input[name='days-counting']");
 	var initDaysCounting = daysCounting;
-	var daysPracticed = getDaysPracticed();
-	var desiredPercent = getDesiredPercentage();
+	var daysPracticed = getValue("input[name='days-practiced']");
+	var desiredPercent = getValue("input[name='desired-percent']");
 	calculateNeededStreak(daysCounting, daysPracticed, desiredPercent, initDaysCounting);
 });
